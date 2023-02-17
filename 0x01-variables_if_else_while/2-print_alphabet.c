7c10f4d97de55db0dde373ce9ff9a8b60a8bf457
endlessbaba <energeticokey@gmail.com>
2023-02-17 12:09:56 -0800

    2-print_alphabet.c

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -3,17 +3,17 @@
 #include<time.h>
 
 /**
- * main - Entry point
+ * main - Prints the alphabet in lowercase.
  *
  * Return: Always 0 (Success)
  */
 
 int main(void)
 {
-char low;
+char letter;
 
-for (low = 'a'; low <= 'z'; low++);
-	putchar(low);
+for (letter = 'a'; letter <= 'z'; letter++)
+	putchar(letter);
 	putchar('\n');	
 return (0);
 }
