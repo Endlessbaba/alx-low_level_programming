ecf46ab93521e3f8280ddbc727213a2adf61f494
endlessbaba <energeticokey@gmail.com>
2023-03-16 15:57:26 -0700

    100-realloc.c

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -32,7 +32,7 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 	if (!ptr1)
 		return (NULL);
 
-	old_size = ptr;
+	old_ptr = ptr;
 
 	if (new_size < old_size)
 	{
@@ -43,7 +43,7 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 	if (new_size > old_size)
 	{
 		for (i = 0; i < old_size; i++)
-			ptr1[i] = old_ptr[i]
+			ptr1[i] = old_ptr[i];
 
 	free(ptr);
 	return (ptr1);
