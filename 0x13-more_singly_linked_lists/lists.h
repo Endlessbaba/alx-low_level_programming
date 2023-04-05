9303eb44545d2f3d42eb770a8479f6f0292d7a74
endlessbaba <energeticokey@gmail.com>
2023-04-04 21:33:24 -0700

    Updated

--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -47,6 +47,8 @@ size_t print_listint_safe(const listint_t *head);
 
 size_t free_listint_safe(listint_t **h);
 
+int pop_listint(listint_t **head);
+
 listint_t *find_listint_loop(listint_t *head);
 
 #endif
